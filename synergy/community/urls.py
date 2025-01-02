ffd9c4eca2e8918cc84239cb27c38738f9bf5a7d
muslmasan <muslmasan3@gmail.com>
2025-01-02 12:34:30 +0200

    finsih some staff

--- synergy/community/urls.py
+++ synergy/community/urls.py
@@ -1,15 +1,10 @@
-from rest_framework.routers import DefaultRouter
-from .views import PostView, CommentView, CommunityView
+# from rest_framework.routers import DefaultRouter
+# from .views import PostView, CommentView, CommunityView
 from django.urls import path, include
 
 
-router = DefaultRouter()
-router.register(r'', CommunityView)
-router.register(r'comment', CommentView)
-router.register(r'post', PostView)
-app_name = 'community'
 
 
-urlpatterns = [
-    path('',include(router.urls)),
-]
+# urlpatterns = [
+#     path('',include(router.urls)),
+# ]
--- synergy/event/models.py
+++ synergy/event/models.py
@@ -1,23 +1,23 @@
-from django.db import models
-from home.models import UserProfile
+# from django.db import models
+# from home.models import UserProfile
 
-typeChoices = (
-    ("PART TIME",'Part time'),
-    ("FULL TIME", "Full time"),
-    ("INTERNERSHIP","Internership"),
-)
+# typeChoices = (
+#     ("PART TIME",'Part time'),
+#     ("FULL TIME", "Full time"),
+#     ("INTERNERSHIP","Internership"),
+# )
 
-class Event(models.Model):
-    title = models.CharField(max_length=256)
-    cover = models.ImageField(upload_to='event_covers')
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    location = models.CharField(max_length=100)
-    time = models.DateTimeField()
-    owner = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
-    type = models.CharField(
-        max_length=100,
-        choices=typeChoices,
+# class Event(models.Model):
+#     title = models.CharField(max_length=256)
+#     cover = models.BinaryField(blank=True)
+#     description = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     location = models.CharField(max_length=100)
+#     time = models.DateTimeField()
+#     owner = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
+#     type = models.CharField(
+#         max_length=100,
+#         choices=typeChoices,
 
-    )
+#     )
